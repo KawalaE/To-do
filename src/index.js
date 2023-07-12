16a09640a6da99adda904ba93b940b05eda76840
KawalaE <edyta.wer.kawala@gmail.com>
2023-07-12 18:46:59 +0200

    Create horizontal navigation class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+import './style.css'
+const page = document.getElementById('page');
+
+class UserInterface{
+
+    static horizontalNav(){
+        const upperNav = document.createElement('div');
+        upperNav.classList.add('upper-nav');
+        page.appendChild(upperNav);
+        const hamburger = document.createElement('div');
+        hamburger.classList.add('hamburger');
+        upperNav.appendChild(hamburger);
+    }
+    
+}
+
+UserInterface.horizontalNav();
+
