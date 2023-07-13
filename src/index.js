391d3c07d59d1f42b95f9a88b20784b50c970bd4
KawalaE <edyta.wer.kawala@gmail.com>
2023-07-13 19:19:14 +0200

    Create a UI layout

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-import './style.css'
-import Favicon from './assets/favicon.png';
-const page = document.getElementById('page');
+import {createUI} from './modules/ui';
 
-class UserInterface{
-
-    static setFavicon(){
-        const myFavicon = new Image();
-        myFavicon.src = Favicon;
-        const head = document.querySelector('head');
-        const favicon = document.createElement('link');
-        favicon.setAttribute('rel', 'shortcut icon');
-        favicon.setAttribute('href', myFavicon.src);
-        head.appendChild(favicon);
-
-    }
-    static horizontalNav(){
-        const upperNav = document.createElement('div');
-        upperNav.classList.add('upper-nav');
-        page.appendChild(upperNav);
-
-        const navigatorName = document.createElement('p');
-        navigatorName.classList.add('navigator-name');
-        navigatorName.textContent = 'To do';
-        upperNav.appendChild(navigatorName);
-
-        const logo = document.createElement('div');
-        logo.classList.add('logo');
-        upperNav.appendChild(logo);
-    }
-}
-
-UserInterface.horizontalNav();
-UserInterface.setFavicon();
-
+createUI();
---
+++ src/modules/ui.js
@@ -0,0 +1,58 @@
+import '/home/Edyta/Desktop/repos/To-do/src/style.css'
+import Favicon from '/home/Edyta/Desktop/repos/To-do/src/assets/favicon.png';
+const page = document.getElementById('page');
+
+class UserInterface{
+
+    static setFavicon(){
+        const myFavicon = new Image();
+        myFavicon.src = Favicon;
+        const head = document.querySelector('head');
+        const favicon = document.createElement('link');
+        favicon.setAttribute('rel', 'shortcut icon');
+        favicon.setAttribute('href', myFavicon.src);
+        head.appendChild(favicon);
+
+    }
+    static horizontalNav(){
+        const upperNav = document.createElement('div');
+        upperNav.classList.add('upper-nav');
+        page.appendChild(upperNav);
+
+        const navigatorName = document.createElement('p');
+        navigatorName.classList.add('navigator-name');
+        navigatorName.textContent = 'To do';
+        upperNav.appendChild(navigatorName);
+
+        const logo = document.createElement('div');
+        logo.classList.add('logo');
+        upperNav.appendChild(logo);
+    }
+    static mainContentArea(){
+        const mainContent = document.createElement('div');
+        mainContent.classList.add('main-content');
+        page.appendChild(mainContent);
+        return mainContent;
+    }
+
+    static projectsNavArea(mainContent){
+        const projectsNav = document.createElement('div');
+        projectsNav.classList.add('projects-nav');
+        mainContent.appendChild(projectsNav);
+        
+    }
+    static taskDisplayArea(mainContent){
+        const taskDisplay = document.createElement('div');
+        taskDisplay.classList.add('task-display');
+        mainContent.appendChild(taskDisplay);
+    }
+}
+ 
+export function createUI(){
+    UserInterface.horizontalNav();
+    UserInterface.setFavicon();
+    const myMainContent =  UserInterface.mainContentArea();
+    UserInterface.projectsNavArea(myMainContent);
+    UserInterface.taskDisplayArea(myMainContent);
+}
+
